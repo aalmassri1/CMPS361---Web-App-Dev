6648c56ac680c1945fcfd7a5423c26e5bc1c9767
aalmassri1 <amalmas@pointpark.edu>
2026-09-06 21:30:32 -0400

    Add Module 1 PHP assignment

---
+++ Module1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CMPS361 - Module1</title>
+    </head>
+    <body>
+        <h1>Welcome to my PHP Webpage</h1>
+
+        <p>Name: Abdelrahman Almasri</p>
+        <p>Course: CMPS361</p>
+        <p>Module 1</p>
+        
+        <p> 
+            <a href="phpinfo.php">View PHP Information</a>
+        </p>
+
+    </body>
+</html>
